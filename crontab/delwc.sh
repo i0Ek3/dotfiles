0cb5042bd2d9bcb60e4a6a6b568849db4247d69f
i0Ek3 <kno30826@gmail.com>
2022-06-06 21:25:08 +0800

    feat: add crontab list

---
+++ crontab/delwc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# remove all wechat files
+rm -rf $HOME/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9
---
+++ crontab/gitpull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# update repos on time
+cd /path-to-your-repo; git pull
